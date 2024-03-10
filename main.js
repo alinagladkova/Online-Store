f323955a295ed71ed9d602942350d087e8c1c9b1
AlinaGladkova <tina.leaf89@gmail.com>
2024-03-10 18:36:49 +0300

    alertList, Alert, Search and debounce

--- main.js
+++ main.js
@@ -204,13 +204,12 @@ class BasicComponent {
 }
 
 class ProductList extends BasicComponent {
-  constructor(products, Product, Choice, ChoiceItem, Popup) {
+  constructor(Product, products, Choice, ChoiceItem) {
     super();
     this._products = products;
     this._Product = Product;
     this._Choice = Choice;
     this._ChoiceItem = ChoiceItem;
-    this._Popup = Popup;
     this._init();
   }
 
@@ -220,11 +219,31 @@ class ProductList extends BasicComponent {
   }
 
   _render() {
-    return this._element.append(...this._addProduct());
+    this._element.append(...this._addProduct());
+  }
+
+  update(inputValue) {
+    this._element.innerHTML = "";
+    this._element.append(...this._getInputValue(inputValue));
+    this._emptyList();
+  }
+
+  _getInputValue(inputValue) {
+    return this._products
+      .filter((product) => {
+        return product.title.toLowerCase().includes(inputValue);
+      })
+      .map((product) => new this._Product(product, this._Choice, this._ChoiceItem, inputValue).element);
+  }
+
+  _emptyList() {
+    if (this._element.innerHTML === "") {
+      return (this._element.innerHTML = `<div class="products-list__empty">Товар не найден</div>`);
+    }
   }
 
   _addProduct() {
-    return this._products.map((product) => new this._Product(product, this._Choice, this._ChoiceItem, this._Popup).element);
+    return this._products.map((product) => new this._Product(product, this._Choice, this._ChoiceItem).element);
   }
 
   _getTemplate() {
@@ -240,8 +259,9 @@ class Product extends BasicComponent {
     isFirstRender: true,
   };
 
-  constructor({ title, price, priceType, description, properties, watts, colors }, Choice, ChoiceItem) {
+  constructor({ id, title, price, priceType, description, properties, watts, colors }, Choice, ChoiceItem, inputValue) {
     super();
+    this._id = id;
     this._title = title;
     this._price = price;
     this._priceType = priceType;
@@ -251,6 +271,7 @@ class Product extends BasicComponent {
     this._colors = colors;
     this._Choice = Choice;
     this._ChoiceItem = ChoiceItem;
+    this._inputValue = inputValue;
     this._init();
   }
 
@@ -266,12 +287,12 @@ class Product extends BasicComponent {
       this._render();
     });
 
-    this._subElements.buy.addEventListener("click", (e) => {
+    this._subElements.imgWrapper.addEventListener("click", (e) => {
       e.target.dispatchEvent(
-        new CustomEvent("addToCart", {
+        new CustomEvent("openImage", {
           bubbles: true,
           detail: {
-            productTitle: this._title,
+            src: this._colors[this._state.colors - 1].img,
           },
         })
       );
@@ -288,11 +309,26 @@ class Product extends BasicComponent {
       );
     });
 
-    this._subElements.imgWrapper.addEventListener("click", (e) => {
+    this._subElements.buy.addEventListener("click", (e) => {
       e.target.dispatchEvent(
-        new CustomEvent("openImage", {
+        new CustomEvent("buy", {
           bubbles: true,
           detail: {
+            productTitle: this._title,
+          },
+        })
+      );
+    });
+
+    this._subElements.buy.addEventListener("click", (e) => {
+      e.target.dispatchEvent(
+        new CustomEvent("addToCart", {
+          bubbles: true,
+          detail: {
+            id: this._id,
+            title: this._title,
+            price: this._price,
+            priceType: this._priceType,
             src: this._colors[this._state.colors - 1].img,
           },
         })
@@ -325,6 +361,16 @@ class Product extends BasicComponent {
     if (this._state.isFirstRender) {
       this._state.isFirstRender = false;
     }
+
+    if (this._title.toLowerCase().includes(this._inputValue)) {
+      this._subElements.title.innerHTML = this._generateTitleMarker();
+    }
+  }
+
+  _generateTitleMarker() {
+    const start = this._title.toLowerCase().indexOf(this._inputValue);
+    const end = start + this._inputValue.length;
+    return `${this._title.slice(0, start)}<span class="product__title-marker">${this._title.slice(start, end)}</span>${this._title.slice(end)}`;
   }
 
   _getWatt(itemId) {
@@ -349,7 +395,7 @@ class Product extends BasicComponent {
           <img class="product__image" data-element="img" src="" alt="img" />
         </div>
         <div class="product__info">
-          <h4 class="product__title">${this._title}</h4>
+          <h4 class="product__title" data-element="title">${this._title}</h4>
           <span class="product__price">${this._price} ${this._priceType}</span>
           <div class="product__options" data-element="options"></div>
           <div class="product__description">${this._description}</div>
@@ -620,7 +666,7 @@ class Header extends BasicComponent {
   constructor(miniCart, Search) {
     super();
     this._miniCart = miniCart;
-    // this._search = new Search();
+    this._search = new Search();
     this._init();
   }
 
@@ -631,7 +677,7 @@ class Header extends BasicComponent {
 
   _render() {
     this._subElements.miniCart.insertAdjacentElement("afterbegin", this._miniCart.element);
-    // this._subElements.search.insertAdjacentElement("afterbegin", this._search.element);
+    this._subElements.search.insertAdjacentElement("afterbegin", this._search.element);
   }
   _getTemplate() {
     return `
@@ -654,33 +700,76 @@ class Header extends BasicComponent {
       	</header>`;
   }
 }
+
 class MiniCart extends BasicComponent {
   _state = {
     active: false,
     totalPrice: 0,
-    totalAmount: [],
+    products: [],
   };
-  constructor() {
+  constructor(MiniCartItem) {
     super();
+    this._MiniCartItem = MiniCartItem;
     this._init();
   }
 
   _init() {
     super._init();
     this._handleClickDocument = this._handleClickDocument.bind(this);
-    this._handleKeykDocument = this._handleKeyDocument.bind(this);
+    this._handleKeyDocument = this._handleKeyDocument.bind(this);
+    this._addListeners();
+    this._render();
   }
 
   _addListeners() {
     this._subElements.wrapper.addEventListener("click", () => {
-      document.addEventListener("click", this._handleClickDocument);
-      document.addEventListener("keydown", this._handleKeyDocument);
       this._setStateActive();
+      this._render();
     });
   }
 
+  addItem(product) {
+    this._state.products.push(product);
+    this._render();
+  }
+
+  removeItem(id) {
+    this._state.products = this._state.products.filter((product) => product.id !== id);
+    this._render();
+  }
+
+  _generateItems() {
+    return this._state.products.map((product) => {
+      return new this._MiniCartItem(product, this.removeItem.bind(this)).element;
+    });
+  }
+
+  _calcTotalProductsAmount() {
+    return this._state.products.length;
+  }
+
+  _calcTotalProductsPrice() {
+    return this._state.products.map((product) => product.price).reduce((acc, el) => acc + el, 0);
+  }
+
   _render() {
+    this._subElements.cartList.innerHTML = "";
+    this._subElements.cartList.append(...this._generateItems());
+
     this._state.active ? this._open() : this._close();
+
+    this._subElements.price.textContent = `${this._calcTotalProductsPrice()} руб`;
+
+    if (this._calcTotalProductsAmount() === 0) {
+      this._subElements.product.textContent = `${this._calcTotalProductsAmount()} товаров`;
+      this._subElements.cartList.textContent = `В корзине пока нет товаров`;
+    } else if (this._calcTotalProductsAmount() === 1) {
+      this._subElements.product.textContent = `${this._calcTotalProductsAmount()} товар`;
+    } else if (this._calcTotalProductsAmount() < 5) {
+      this._subElements.product.textContent = `${this._calcTotalProductsAmount()} товара`;
+    } else {
+      this._subElements.product.textContent = `${this._calcTotalProductsAmount()} товаров`;
+    }
   }
 
   _setStateActive() {
@@ -689,16 +778,20 @@ class MiniCart extends BasicComponent {
 
   _handleClickDocument(e) {
     if (!e.target.closest(".mini-cart")) {
+      this._setStateActive();
       this._close();
     }
   }
 
   _handleKeyDocument(e) {
     if (e.key === "Escape") {
+      this._setStateActive();
       this._close();
     }
   }
   _open() {
+    document.addEventListener("click", this._handleClickDocument);
+    document.addEventListener("keydown", this._handleKeyDocument);
     this._subElements.productsWrapper.classList.add("mini-cart__products-wrapper--active");
   }
 
@@ -729,19 +822,294 @@ class MiniCart extends BasicComponent {
 		`;
   }
 }
-class MiniCartItem extends BasicComponent {}
-class AlertList extends BasicComponent {}
-class Alert extends BasicComponent {}
-class Search extends BasicComponent {}
+
+class MiniCartItem extends BasicComponent {
+  constructor({ id, title, price, priceType, src }, removeItemCallback) {
+    super();
+    this._removeItemCallback = removeItemCallback;
+    this._id = id;
+    this._title = title;
+    this._price = price;
+    this._priceType = priceType;
+    this._src = src;
+    this._init();
+  }
+
+  _init() {
+    super._init();
+    this._addListeners();
+    this._render();
+  }
+
+  _addListeners() {
+    this._subElements.close.addEventListener("click", (e) => {
+      e.target.dispatchEvent(
+        new CustomEvent("removeMiniCartItem", {
+          bubbles: true,
+          detail: {
+            title: this._title,
+            price: this._price,
+            priceType: this._priceType,
+          },
+        })
+      );
+    });
+
+    this._subElements.close.addEventListener("click", () => {
+      this._removeItemCallback(this._id);
+      this._remove();
+    });
+  }
+
+  _render() {
+    this._subElements.productTitle.textContent = `${this._title}`;
+    this._subElements.productAmount.textContent = `${this._price} ${this._priceType}`;
+    this._subElements.productImage.src = `img/${this._src}`;
+  }
+  _remove() {
+    this._element.remove();
+  }
+
+  _getTemplate() {
+    return `
+		    <div class="mini-item">
+					<div class="mini-item__image" >
+						<img src="" data-element="productImage" alt="image" />
+					</div>
+					<div class="mini-item__details">
+						<div class="mini-item__title" data-element="productTitle"></div>
+						<div class="mini-item__amount" data-element="productAmount"></div>
+					</div>
+					<button class="btn btn--closeItem" data-element="close">x</button>
+    		</div>
+		`;
+  }
+}
+class AlertList extends BasicComponent {
+  _state = {
+    alertQueue: [],
+  };
+  constructor() {
+    super();
+    this._init();
+  }
+
+  _init() {
+    super._init();
+    this._addListeners();
+  }
+
+  _addListeners() {
+    this._subElements.btnHide.addEventListener("click", () => {
+      this._state.alertQueue.forEach((alert) => {
+        this._removeAlert(alert);
+        alert.destroy();
+      });
+    });
+  }
+
+  addAlert(product) {
+    this._state.alertQueue.push(product);
+    this._subElements.list.innerHTML = "";
+    this._subElements.list.append(...this._generateAlerts());
+    this._render();
+  }
+
+  _calcAlertNumber() {
+    return this._state.alertQueue.length;
+  }
+
+  _calcHeight() {
+    this._subElements.list.style.height = `${2 * 1 + this._state.alertQueue.slice(0, 1).reduce((acc, el) => acc + el.getSize().height, 0)}px`;
+  }
+
+  _generateAlerts() {
+    return this._state.alertQueue.map((alert) => {
+      return alert.render(this._removeAlert.bind(this));
+    });
+  }
+
+  _setActiveState() {
+    if (this._calcAlertNumber() > 1) {
+      this._subElements.control.classList.add("remove-list__control--active");
+      this._calcHeight();
+    } else {
+      this._subElements.control.classList.remove("remove-list__control--active");
+      this._subElements.list.style.height = "auto";
+    }
+  }
+
+  _render() {
+    this._setActiveState();
+
+    this._subElements.number.textContent = `+${this._calcAlertNumber() - 1}`;
+  }
+
+  _removeAlert(context) {
+    this._state.alertQueue = this._state.alertQueue.filter((alert) => context !== alert);
+    this._render();
+  }
+
+  _getTemplate() {
+    return `<div class="remove-list">
+							<div class="remove-list__control" data-element="control">
+								<div class="remove-list__triangle">
+									<span class="remove-list__number-alerts" data-element="number"></span>
+								</div>
+								<button class="btn btn--hide remove-list__hide-alerts" data-element="btnHide">Скрыть всё</button>
+							</div>
+							<div class="remove-list__list" data-element="list"></div>
+						</div>`;
+  }
+}
+class Alert extends BasicComponent {
+  _timerId;
+  _callback;
+
+  constructor({ title, price, priceType }) {
+    super();
+    this._title = title;
+    this._price = price;
+    this._priceType = priceType;
+
+    this._init();
+  }
+
+  _init() {
+    super._init();
+    this.render();
+  }
+
+  _createTimer() {
+    this._timerId = setTimeout(() => {
+      this._remove();
+      this._callback(this);
+    }, 5000);
+  }
+
+  _remove() {
+    this._element.remove();
+  }
+
+  destroy() {
+    clearTimeout(this._timerId);
+    return this._remove();
+  }
+
+  getSize() {
+    return {
+      height: this._element.getBoundingClientRect().height,
+      width: this._element.getBoundingClientRect().width,
+    };
+  }
+
+  render(callback) {
+    this._callback = callback;
+    this._createTimer();
+    return this._element;
+  }
+
+  _getTemplate() {
+    return `<div class="remove-alert">
+							<div class="remove-alert__timer"></div>
+								<p class="remove-alert__text" data-element="text">
+									<span class="remove-alert__product-title" data-element="data">${this._title} за ${this._price}${this._priceType}</span>
+									был удалён из корзины.
+								</p>
+        		</div>`;
+  }
+}
+class Search extends BasicComponent {
+  _state = {
+    active: false,
+    input: "",
+  };
+
+  constructor() {
+    super();
+    this._init();
+  }
+
+  _init() {
+    super._init();
+    this._handleKeyDocument = this._handleKeyDocument.bind(this);
+    this._handleClickDocument = this._handleClickDocument.bind(this);
+    this._addListeners();
+  }
+
+  _addListeners() {
+    this._subElements.icon.addEventListener("click", () => {
+      document.addEventListener("keydown", this._handleKeyDocument);
+      document.addEventListener("click", this._handleClickDocument);
+      this._setActiveState();
+      this._render();
+    });
+
+    this._subElements.field.addEventListener("input", (e) => {
+      e.target.dispatchEvent(
+        new CustomEvent("getInput", {
+          bubbles: true,
+          detail: {
+            inputValue: e.target.value.trim(),
+          },
+        })
+      );
+    });
+  }
+
+  _setActiveState() {
+    this._state.active = !this._state.active;
+  }
+
+  _open() {
+    this._subElements.wrapper.classList.add("search__wrapper--active");
+  }
+
+  _handleKeyDocument(e) {
+    if (e.key === "Escape") {
+      this._close();
+    }
+  }
+
+  _handleClickDocument(e) {
+    if (!e.target.closest(".search")) {
+      this._close();
+    }
+  }
+
+  _close() {
+    document.removeEventListener("keydown", this._handleKeyDocument);
+    document.removeEventListener("click", this._handleClickDocument);
+    this._subElements.wrapper.classList.remove("search__wrapper--active");
+  }
+
+  _render() {
+    this._state.active ? this._open() : this._close();
+  }
+
+  _getTemplate() {
+    return `<div class="search">
+							<div class="search__icon" data-element="icon">
+								<i class="fa-solid fa-magnifying-glass"></i>
+							</div>
+							<div class="search__wrapper" data-element="wrapper">
+								<form class="search__form" action="#">
+									<input class="search__field" type="text" data-element="field" placeholder="Ищите товар"/>
+								</form>
+							</div>
+    				</div>`;
+  }
+}
 
 const root = document.querySelector(".root");
+const productList = new ProductList(Product, products, Choice, ChoiceItem);
 const popupBuy = new PopupBuy();
 const popupProperties = new PopupProperties(PropertyItem);
 const popupImage = new PopupImage();
+const miniCart = new MiniCart(MiniCartItem);
+const alertList = new AlertList();
 
-const miniCart = new MiniCart();
-
-root.addEventListener("addToCart", (e) => {
+root.addEventListener("buy", (e) => {
   popupBuy.open(e.detail.productTitle);
 });
 
@@ -753,8 +1121,43 @@ root.addEventListener("openImage", (e) => {
   popupImage.open(e.detail.src);
 });
 
-root.insertAdjacentElement("afterbegin", new Header(miniCart).element);
-root.insertAdjacentElement("beforeend", new ProductList(products, Product, Choice, ChoiceItem).element);
+root.addEventListener("addToCart", (e) => {
+  miniCart.addItem(e.detail);
+});
+
+root.addEventListener("removeMiniCartItem", (e) => {
+  alertList.addAlert(
+    new Alert({
+      title: e.detail.title,
+      price: e.detail.price,
+      priceType: e.detail.priceType,
+    })
+  );
+});
+
+root.addEventListener("getInput", (e) => {
+  const searchFieldHandler = debounce(() => productList.update(e.detail.inputValue), 3000);
+  searchFieldHandler();
+});
+
+//================Debounce
+function debounce(handler, ms) {
+  let timeoutID;
+  return (...args) => {
+    clearTimeout(timeoutID);
+
+    timeoutID = setTimeout(() => {
+      timeoutID = null;
+      return handler.apply(this, args);
+    }, ms);
+  };
+}
+
+root.insertAdjacentElement("beforeend", productList.element);
+root.insertAdjacentElement("afterbegin", new Header(miniCart, Search).element);
 root.insertAdjacentElement("afterbegin", popupBuy.element);
 root.insertAdjacentElement("afterbegin", popupProperties.element);
 root.insertAdjacentElement("afterbegin", popupImage.element);
+root.insertAdjacentElement("beforeend", alertList.element);
+
+//удаляет 2 товара из миникарта при повторном клике, исправить
